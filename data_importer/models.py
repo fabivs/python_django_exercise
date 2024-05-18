fa6ab932f96fd5625b375986b60ffb779e91c27b
Fabio Prizio <fabio.prizio@casavo.com>
2024-05-18 17:40:41 +0200

    feat: add Report model with migration

--- data_importer/models.py
+++ data_importer/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Report(models.Model):
+    date = models.DateField()
+    restaurant = models.CharField(max_length=100)
+    planned_hours = models.IntegerField()
+    actual_hours = models.IntegerField()
+    budget = models.DecimalField(max_digits=10, decimal_places=2)
+    sells = models.DecimalField(max_digits=10, decimal_places=2)
+
+@property
+def hours_difference(self):
+    return self.planned_hours - self.actual_hours
+
+@property
+def budget_difference(self):
+    return self.budget - self.sells
--- simple_web_app/settings.py
+++ simple_web_app/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'data_importer.apps.DataImporterConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
