0709d5319b2e7a096d7003fc2ac05b6b74309459
Fabio Prizio <fabio.prizio@casavo.com>
2024-05-20 01:47:59 +0200

    refactor: project name

--- python_django_exercise/urls.py
+++ python_django_exercise/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for simple_web_app project.
+URL configuration for python_django_exercise project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
--- python_django_exercise/wsgi.py
+++ python_django_exercise/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for simple_web_app project.
+WSGI config for python_django_exercise project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "simple_web_app.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "python_django_exercise.settings")
 
 application = get_wsgi_application()
