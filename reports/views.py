d72c9f33979da9674de22c83d524d4702db7d7f2
Fabio Prizio <fabio.prizio@casavo.com>
2024-05-21 20:08:08 +0200

    feat: filter API query by restaurant name

--- reports/views.py
+++ reports/views.py
@@ -1,15 +1,28 @@
-from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 from .models import Report
 from .serializers import ReportSerializer
+from django_filters import rest_framework as filters
 
 
-class ListReportsView(APIView):
+class ReportFilter(filters.FilterSet):
+    restaurant = filters.CharFilter(lookup_expr="exact")
+
+    class Meta:
+        model = Report
+        fields = ["restaurant"]
+
+
+class ListReportsView(ListAPIView):
+    queryset = Report.objects.all().order_by("id")
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = ReportFilter
+
     def get(self, request, format=None):
         """
         Return a list of all reports.
         """
 
-        reports = Report.objects.all().order_by("id")
-        serializer = ReportSerializer(reports, many=True)
+        filtered_reports = self.filter_queryset(self.queryset)
+        serializer = ReportSerializer(filtered_reports, many=True)
         return Response(serializer.data)
